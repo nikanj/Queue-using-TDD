94cfe0f0807a2e90dbe1762f3224d110b4fd9fd3
nikanjgupta <nikanj.gupta@valtech.de>
2017-07-07 22:35:30 +0200

    added first test to verify creation of empty class.

---
+++ src/test/QueueTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class QueueTest {
+
+	@Test
+	public void createQueueClass() {
+		assertNotNull(new QueueClass());
+	}
+}
